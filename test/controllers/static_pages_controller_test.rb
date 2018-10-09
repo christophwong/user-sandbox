c92419a490b7ee53c9064827e2ceb856a5196de6
Christoph Wong <wong.christoph@gmail.com>
2018-10-08 23:38:23 -0700

    rollback authentication functionality based on Devise

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,17 +9,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect user if not signed in" do
+  skip
     get static_pages_members_only_url
     assert_redirected_to new_user_session_path
   end
 
   test "should get members only page after sign in" do
+  skip
     sign_in users(:one)
     get static_pages_members_only_url
     assert_response :success
   end
 
   test "should get admin_only" do
+  skip
     sign_in users(:one)
     get static_pages_admin_only_url
     assert_redirected_to root_path
