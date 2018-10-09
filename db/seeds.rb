b5da28c5c508deb52d00a5f91885af6530b741e3
Christoph Wong <wong.christoph@gmail.com>
2018-10-08 17:17:50 -0700

    add warning for learners that read this repo as example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 #Seeding admin
+=begin
+Warning: in case anyone is learning from this repo, 
+this is not safe to do on real application as it exposes 
+an admin password in plain text. Better not source control this file.
+=end
+
 User.create(
   :email => "admin@test.com",
   :password => "admin123",
